d5828b36202a60eb91cccdc1e81a0ec0d076502f
Alex Lapinski <github@alexlapinski.name>
2012-09-09 21:41:34 -0400

    jslint-ified

--- src/jcx.js
+++ src/jcx.js
@@ -1,24 +1,27 @@
-define(function() {
+/*jslint nomen: true */
+/*global define*/
+
+define(function () {
 
     "use strict";
 
-     // a wrapper for normal html5 canvas context
-     function JCX(context) {
+    // a wrapper for normal html5 canvas context
+    function JCX(context) {
 
-         Object.defineProperties(this,
-         {
-             _innerContext : {
-                 value: context,
-                 writable: false,
-                 configurable: false,
-                 enumerable: false
-             },
-         });
-     }
+        Object.defineProperties(this, {
+            _innerContext : {
+                value: context,
+                writable: false,
+                configurable: false,
+                enumerable: false
+            }
+        });
+    }
 
     JCX.prototype = {
-        drawRectangle: function(x, y, width, height, color, fillColor) {
-            if( fillColor ) {
+
+        drawRectangle: function (x, y, width, height, color, fillColor) {
+            if (fillColor) {
                 this._innerContext.fillStyle = fillColor;
                 this._innerContext.fillRect(x, y, width, height);
             }
@@ -27,13 +30,13 @@ define(function() {
             this._innerContext.strokeRect(x, y, width, height);
         },
 
-        drawSquare: function(x, y, size, color, fillColor) {
+        drawSquare: function (x, y, size, color, fillColor) {
             this.drawRectangle(x, y, size, size, color, fillColor);
         },
-        drawCircle: function(x, y, radius, color, fillColor) {
-            
+
+        drawCircle: function (x, y, radius, color, fillColor) {
             this._innerContext.beginPath();
-            if( fillColor ) {
+            if (fillColor) {
                 this._innerContext.fillStyle = fillColor;
                 this._innerContext.arc(x, y, radius, 0, 360);
                 this._innerContext.fill();
@@ -43,19 +46,24 @@ define(function() {
             this._innerContext.stroke();
             this._innerContext.closePath();
         },
-        drawText: function(x, y, text, color, fillColor){
+
+        drawText: function (x, y, text, color, fillColor) {
             //y is the bottom (baseline?) of the text by default
-            this._innerContext.font = "900 14px/2 Consolas";
-            if(fillColor){
+            this._innerContext.font = "900 14px/2 Consolas"; // TODO: Extract the setting of font
+
+            if (fillColor) {
                 this._innerContext.fillStyle = fillColor;
                 this._innerContext.fillText(text, x, y);
             }
+
             this._innerContext.strokeStyle = color;
-            this._innerContext.strokeText(text,x,y);
+            this._innerContext.strokeText(text, x, y);
         },
-        clearRect: function(x, y, width, height){
-            this._innerContext.clearRect(x,y,width,height);
+
+        clearRect: function (x, y, width, height) {
+            this._innerContext.clearRect(x, y, width, height);
         }
     };
+
     return JCX;
 });
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+/*global require*/
+
 require.config({
 });
 
