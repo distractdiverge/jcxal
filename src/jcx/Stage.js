f20bf6f45e5c8bd42af06de05b324ab359767426
Alex Lapinski <github@alexlapinski.name>
2012-09-11 11:55:25 -0400

    jslint-ified

--- src/jcx/Stage.js
+++ src/jcx/Stage.js
@@ -1,72 +1,77 @@
+/*global define*/
+
 // A canvas wrapper for object drawing
-define(['jcx', 'jcx/DisplayObjectContainer', 'jcx/MouseEvent'], function(JCX, DisplayObjectContainer, MouseEvent){
+define(['jcx', 'jcx/DisplayObjectContainer', 'jcx/MouseEvent'], function (JCX, DisplayObjectContainer, MouseEvent) {
+
+    "use strict";
 
     function Stage(canvas) {
 
         DisplayObjectContainer.call(this);
-        Object.defineProperties(this,{
-            color:{
-                value:"#ffffff",
-                writable:true,
-                configurable:false,
+
+        Object.defineProperties(this, {
+            color: {
+                value: "#ffffff",
+                writable: true,
+                configurable: false,
                 enumerable: true
             },
-            constructor:{
-                value:null,
-                writable:true,
-                configurable:false,
-                enumerable:true
+            constructor: {
+                value: null,
+                writable: true,
+                configurable: false,
+                enumerable: true
             },
-            focus:{
-                value:null,
-                writable:true,
-                configurable:false,
-                enumerable:true
+            focus: {
+                value: null,
+                writable: true,
+                configurable: false,
+                enumerable: true
             },
-            frameRate:{
-                value:null,
-                writable:true,
-                configurable:false,
-                enumerable:true
-            },
-            //@override
-            mouseChildren:{
-                value:null,
-                writable:true,
-                configurable:false,
-                enumerable:true
-            },
-            nativeWindow:{
-                value:null,
-                writable:false,
-                configurable:false,
-                enumerable:true
+            frameRate: {
+                value: null,
+                writable: true,
+                configurable: false,
+                enumerable: true
             },
             //@override
-            numChildren:{
-                value:null,
-                writable:false,
-                configurable:false,
-                enumerable:true
+            mouseChildren: {
+                value: null,
+                writable: true,
+                configurable: false,
+                enumerable: true
+            },
+            nativeWindow: {
+                value: null,
+                writable: false,
+                configurable: false,
+                enumerable: true
             },
             //@override
-            tabChildren:{
-                value:null,
-                writable:true,
-                configurable:false,
-                enumerable:true
+            numChildren: {
+                value: null,
+                writable: false,
+                configurable: false,
+                enumerable: true
+            },
+            //@override
+            tabChildren: {
+                value: null,
+                writable: true,
+                configurable: false,
+                enumerable: true
             },
             stageX: {
-                value:0,
-                writable:false,
-                configurable:false,
-                enumerable:false
+                value: 0,
+                writable: false,
+                configurable: false,
+                enumerable: false
             },
             stageY: {
-                value:0,
-                writable:false,
-                configurable:false,
-                enumerable:false
+                value: 0,
+                writable: false,
+                configurable: false,
+                enumerable: false
             }
         });
 
@@ -80,52 +85,52 @@ define(['jcx', 'jcx/DisplayObjectContainer', 'jcx/MouseEvent'], function(JCX, Di
 
         // setup click handler
         var that = this;
-        canvas.onclick = function(e) {
+        canvas.onclick = function (e) {
             //TODO: this should function should first find the target via graph traversal,
             //search for a cached path to it, and then execute the event path
             var mouseClickEvent = new MouseEvent(MouseEvent.CLICK, false, false, e.x, e.y);
             mouseClickEvent.target = that;
             that.notifyClick(mouseClickEvent);
         };
-        canvas.onmousedown = function(e){
+        canvas.onmousedown = function (e) {
             //TODO: this should function should first find the target via graph traversal,
             //search for a cached path to it, and then execute the event path
             var mouseDownEvent = new MouseEvent(MouseEvent.MOUSE_DOWN, false, false, e.x, e.y);
             mouseDownEvent.target = that;
             that.notifyMouseDown(mouseDownEvent);
         };
-        canvas.onmouseup = function(e){
+        canvas.onmouseup = function (e) {
             //TODO: this should function should first find the target via graph traversal,
             //search for a cached path to it, and then execute the event path
             var mouseUpEvent = new MouseEvent(MouseEvent.MOUSE_UP, false, false, e.x, e.y);
             mouseUpEvent.target = that;
             that.notifyMouseUp(mouseUpEvent);
         };
-        canvas.onmousemove = function(e){
+        canvas.onmousemove = function (e) {
             //TODO: this should function should first find the target via graph traversal,
             //search for a cached path to it, and then execute the event path
-            var mouseMoveEvent = new MouseEvent(MouseEvent.MOUSE_MOVE, false, false, e.x, e.y)
+            var mouseMoveEvent = new MouseEvent(MouseEvent.MOUSE_MOVE, false, false, e.x, e.y);
             mouseMoveEvent.target = that;
             that.notifyMouseMove(mouseMoveEvent);
         };
     }
-    
+
     Stage.prototype = new DisplayObjectContainer();
 
     //@override
-    Stage.prototype.draw = function(){
+    Stage.prototype.draw = function () {
         this.context.clearRect(0, 0, this.width, this.height);
         DisplayObjectContainer.prototype.draw.call(this);
     };
 
     //@override
-    Stage.prototype.addChild = function(item){
+    Stage.prototype.addChild = function (item) {
         DisplayObjectContainer.prototype.addChild.call(this, item);
         item.stage = this;
     };
 
     //@override
-    Stage.prototype.addChildAt = function(item, index){
+    Stage.prototype.addChildAt = function (item, index) {
         DisplayObjectContainer.prototype.addChildAt.call(this, item, index);
         item.stage = this;
     };
