a2d2f4b5f0a4ac12c71b5531e0d78ae6fb33793b
Alex Lapinski <github@alexlapinski.name>
2012-09-11 11:56:48 -0400

    jslint-ified

--- src/jcx/StaticText.js
+++ src/jcx/StaticText.js
@@ -1,23 +1,25 @@
-define(['jcx/RenderableObject', 'jcx'],function(RenderableObject, JCX){
+/*global define*/
+
+define(['jcx/RenderableObject', 'jcx'], function (RenderableObject, JCX) {
+
     "use strict";
 
-    function StaticText(){
-        //need to do something about this - is duplicated in Shape
-        var _jcx;
-        Object.defineProperties(this,{
-            text:{
-                value:"",
-                writable:true,
-                configurable:false,
-                enumerable:true
+    function StaticText() {
+        // TODO: need to do something about this - is duplicated in Shape
+        Object.defineProperties(this, {
+            text: {
+                value: "",
+                writable: true,
+                configurable: false,
+                enumerable: true
             }
         });
-        
+
         RenderableObject.call(this);
     }
 
     StaticText.prototype = new RenderableObject();
-    StaticText.prototype.draw = function(){
+    StaticText.prototype.draw = function () {
         this.renderer(this.jcx);
         RenderableObject.prototype.draw.call(this);
     };
