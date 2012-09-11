6c187cb696d7658d4ad6e9cd4ef285b5516541f0
Alex Lapinski <github@alexlapinski.name>
2012-09-11 08:50:59 -0400

    jslint-ified

--- src/jcx/sprite.js
+++ src/jcx/sprite.js
@@ -1,35 +1,39 @@
-define(['jcx/DisplayObjectContainer'], function(DisplayObjectContainer){
+/*jslint nomen: true*/
+/*global define*/
+
+define(['jcx/DisplayObjectContainer'], function (DisplayObjectContainer) {
 
     "use strict";
 
     function Sprite() {
-        this._isBeingTouchDragged = false;
+        this._isBeingTouchDragged = false; // TODO: Replace these properties with internal variables & Object definedProperties
         this._isBeingDragged = false;
         Object.defineProperties(this, {
-            isBeingDragged:{
-                get:function(){return this._isBeingDragged;},
-                configurable:false,
-                enumerable:false
+            isBeingDragged: {
+                get: function () {return this._isBeingDragged; },
+                configurable: false,
+                enumerable: false
             }
         });
+
         DisplayObjectContainer.call(this);
     }
 
     Sprite.prototype = new DisplayObjectContainer();
-    
-    Sprite.prototype.startDrag = function(lockCenter, bounds){
+
+    Sprite.prototype.startDrag = function (lockCenter, bounds) {
         this._isBeingDragged = true;
     };
 
-    Sprite.prototype.startTouchDrag = function(touchPointID, lockCenter, bounds){
-        this._isBeingTouchDragged= true;
+    Sprite.prototype.startTouchDrag = function (touchPointID, lockCenter, bounds) {
+        this._isBeingTouchDragged = true;
     };
 
-    Sprite.prototype.stopDrag = function(){
+    Sprite.prototype.stopDrag = function () {
         this._isBeingDragged = false;
     };
 
-    Sprite.prototype.stopTouchDrag = function(){
+    Sprite.prototype.stopTouchDrag = function () {
         this._isBeingTouchDragged = false;
     };
     return Sprite;
